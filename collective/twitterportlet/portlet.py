859bed3e1cd5dac544e76ee4164cb223aa5609d2
benglynn <benglynn@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-27 10:03:44 +0000

    Added expand method which will covert urls, #tags and @names in tweets to HTML anchors
    git-svn-id: http://svn.plone.org/svn/collective/collective.twitterportlet/trunk@96012 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/twitterportlet/portlet.py
+++ collective/twitterportlet/portlet.py
@@ -7,6 +7,7 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.twitterportlet import TwitterPortletMessageFactory as _
 from plone.memoize.instance import memoize
 import twitter
+import re
 
 
 class ITwitterPortlet(IPortletDataProvider):
@@ -50,6 +51,38 @@ class Renderer(base.Renderer):
     @property
     def available(self):
         return True
+
+    # Match and capture urls
+    urlsRegexp = re.compile(r"""
+        (
+        http://                 # Protocol
+        [A-Za-z0-9\-/.]*        # Alphanumeric, dash, slah or dot
+        [A-Za-z0-9\-/]+         # Don't end with a dot
+        )
+        """, re.VERBOSE)
+    
+    # Match and capture #tags
+    hashRegexp = re.compile(r"""
+            \#([A-Za-z0-9\-]+)  # Hash followed by at least one alphanumeric or dash
+            """, re.VERBOSE)
+    
+    # Match and capture @names
+    atRegexp = re.compile(r"""
+            @([A-Za-z0-9\-]+)   # At symbol followed by at least one alphanumeric or dash
+            """, re.VERBOSE)
+    
+    def expand(self, str):
+        str = re.sub(self.urlsRegexp, '<a href="\g<1>">\g<1></a>', str)
+        str = re.sub(self.hashRegexp, '<a href="http://twitter.com/search?q=%23\g<1>">#\g<1></a>', str)
+        str = re.sub(self.atRegexp, '<a href="http://twitter.com/\g<1>">@\g<1></a>', str) 
+        return str
+    
+    # Expand urls, #tags and @names with HTML anchors
+    def expand2(self, str):
+        str = re.sub(urlsRegexp, '<a href="\g<1>">\g<1></a>', str)
+        str = re.sub(hashRegexp, '<a href="http://twitter.com/search?q=%23\g<1>">#\g<1></a>', str)
+        str = re.sub(atRegexp, '<a href="http://twitter.com/\g<1>">@\g<1></a>', str) 
+        return str
     
     @memoize
     def get_tweets(self):
@@ -60,7 +93,7 @@ class Renderer(base.Renderer):
             return twapi.GetUserTimeline(username)[:limit]
         except:
             return []
-
+            
 
 class AddForm(base.AddForm):
     """Portlet add form"""
