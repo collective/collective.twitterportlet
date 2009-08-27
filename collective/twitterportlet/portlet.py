7422d5e47d4ed4063b4619b8fa5101abb7525b92
sparcd <sparcd@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-27 11:38:14 +0000

    Added tests and moved the expand function out so it can be tested.
    git-svn-id: http://svn.plone.org/svn/collective/collective.twitterportlet/trunk@96025 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/twitterportlet/portlet.py
+++ collective/twitterportlet/portlet.py
@@ -10,6 +10,36 @@ import twitter
 import re
 
 
+# Match and capture urls
+urlsRegexp = re.compile(r"""
+    (
+    http://                 # Protocol
+    [A-Za-z0-9\-/.]*        # Alphanumeric, dash, slash or dot
+    [A-Za-z0-9\-/]+         # Don't end with a dot
+    )
+    """, re.VERBOSE)
+
+# Match and capture #tags
+hashRegexp = re.compile(r"""
+        \#([A-Za-z0-9\-]+)  # Hash followed by at least one alphanumeric or dash
+        """, re.VERBOSE)
+
+# Match and capture @names
+atRegexp = re.compile(r"""
+        @([A-Za-z0-9\-]+)   # At symbol followed by at least one alphanumeric or dash
+        """, re.VERBOSE)
+        
+
+def expand_tweet(str):
+    """This method takes a string, parses it for URLs, hashtags and mentions
+       and returns a hyperlinked string.
+    """
+    str = re.sub(urlsRegexp, '<a href="\g<1>">\g<1></a>', str)
+    str = re.sub(hashRegexp, '<a href="http://twitter.com/search?q=%23\g<1>">#\g<1></a>', str)
+    str = re.sub(atRegexp, '<a href="http://twitter.com/\g<1>">@\g<1></a>', str) 
+    return str
+        
+
 class ITwitterPortlet(IPortletDataProvider):
     """A twitter portlet"""
 
@@ -51,31 +81,9 @@ class Renderer(base.Renderer):
     @property
     def available(self):
         return True
-
-    # Match and capture urls
-    urlsRegexp = re.compile(r"""
-        (
-        http://                 # Protocol
-        [A-Za-z0-9\-/.]*        # Alphanumeric, dash, slah or dot
-        [A-Za-z0-9\-/]+         # Don't end with a dot
-        )
-        """, re.VERBOSE)
-    
-    # Match and capture #tags
-    hashRegexp = re.compile(r"""
-            \#([A-Za-z0-9\-]+)  # Hash followed by at least one alphanumeric or dash
-            """, re.VERBOSE)
-    
-    # Match and capture @names
-    atRegexp = re.compile(r"""
-            @([A-Za-z0-9\-]+)   # At symbol followed by at least one alphanumeric or dash
-            """, re.VERBOSE)
     
     def expand(self, str):
-        str = re.sub(self.urlsRegexp, '<a href="\g<1>">\g<1></a>', str)
-        str = re.sub(self.hashRegexp, '<a href="http://twitter.com/search?q=%23\g<1>">#\g<1></a>', str)
-        str = re.sub(self.atRegexp, '<a href="http://twitter.com/\g<1>">@\g<1></a>', str) 
-        return str
+        return expand_tweet(str)
     
     @memoize
     def get_tweets(self):
