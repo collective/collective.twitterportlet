834e0d11ef672d560d5ecdc7842f697572f293e0
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-21 12:21:03 +0000

    Added z3c.autoinclude.plugin entry point, so in Plone 3.3+ you can avoid loading the ZCML file.
    git-svn-id: http://svn.plone.org/svn/collective/collective.twitterportlet/trunk@99965 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='collective.twitterportlet',
       description="A Twitter portlet for Plone.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
@@ -27,9 +26,9 @@ setup(name='collective.twitterportlet',
           'setuptools',
           'python-twitter',
           'simplejson'
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
