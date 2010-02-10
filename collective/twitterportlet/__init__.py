0e61261755e6824bcfd5f9e9346630803f2ac3a6
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-02-10 18:40:44 +0000

    Removed the empty initialize method.
    git-svn-id: http://svn.plone.org/svn/collective/collective.twitterportlet/trunk@110668 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/twitterportlet/__init__.py
+++ collective/twitterportlet/__init__.py
@@ -1,5 +1,2 @@
 from zope.i18nmessageid import MessageFactory
 TwitterPortletMessageFactory = MessageFactory('collective.twitterportlet')
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
